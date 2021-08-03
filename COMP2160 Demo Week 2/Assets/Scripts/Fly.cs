800d9f0365edfbe597da13958dcf1d74c943a6af
Malcolm Ryan <malcolm.ryan@gmail.com>
2021-08-03 11:16:47 +1000

    Working rotation with euler angles

--- COMP2160 Demo Week 2/Assets/Scripts/Fly.cs
+++ COMP2160 Demo Week 2/Assets/Scripts/Fly.cs
@@ -5,8 +5,7 @@ using UnityEngine;
 public class Fly : MonoBehaviour
 {
     public float speed = 30;        // m/s
-    public float rotationSpeed = 60;    // deg/s
-    public Vector3 axis = Vector3.up;
+    public float rotateSpeed = 60;    // deg/s
 
     void Start()
     {
@@ -20,16 +19,10 @@ public class Fly : MonoBehaviour
         transform.Translate(forward * speed * Vector3.forward * Time.deltaTime, Space.Self);
 
         // rotate
-        float roll = Input.GetAxis(InputAxes.RotateRoll);
-        float pitch = Input.GetAxis(InputAxes.RotatePitch);
-        float heading = Input.GetAxis(InputAxes.RotateHeading);
+        float roll = Input.GetAxis(InputAxes.RotateRoll) * rotateSpeed * Time.deltaTime;
+        float pitch = Input.GetAxis(InputAxes.RotatePitch) * rotateSpeed * Time.deltaTime;
+        float heading = Input.GetAxis(InputAxes.RotateHeading) * rotateSpeed * Time.deltaTime;
 
-        Quaternion qRoll = Quaternion.AngleAxis(roll, Vector3.forward);
-        Quaternion qPitch = Quaternion.AngleAxis(pitch, Vector3.right);
-        Quaternion qHeading = Quaternion.AngleAxis(heading, Vector3.up);
-
-        Quaternion q = qHeading * qPitch * qRoll;
-        transform.rotation = q * transform.rotation; 
-
+        transform.localEulerAngles = transform.localEulerAngles + new Vector3(pitch, heading, roll);
     }
 }
