de43bb1c8ca50ddffc1a85f9e93f6c471f6eb7e5
Malcolm Ryan <malcolm.ryan@gmail.com>
2021-08-03 11:04:55 +1000

    Added rotation (broken)

--- COMP2160 Demo Week 2/Assets/Scripts/Fly.cs
+++ COMP2160 Demo Week 2/Assets/Scripts/Fly.cs
@@ -4,7 +4,9 @@ using UnityEngine;
 
 public class Fly : MonoBehaviour
 {
-    public float speed = 30;
+    public float speed = 30;        // m/s
+    public float rotationSpeed = 60;    // deg/s
+    public Vector3 axis = Vector3.up;
 
     void Start()
     {
@@ -13,7 +15,21 @@ public class Fly : MonoBehaviour
 
     void Update()
     {
-        float fwd = Input.GetAxis(InputAxes.Forward);
-        transform.Translate(fwd * speed * Vector3.forward * Time.deltaTime, Space.Self);
+        // move forward
+        float forward = Input.GetAxis(InputAxes.Forward);
+        transform.Translate(forward * speed * Vector3.forward * Time.deltaTime, Space.Self);
+
+        // rotate
+        float roll = Input.GetAxis(InputAxes.RotateRoll);
+        float pitch = Input.GetAxis(InputAxes.RotatePitch);
+        float heading = Input.GetAxis(InputAxes.RotateHeading);
+
+        Quaternion qRoll = Quaternion.AngleAxis(roll, Vector3.forward);
+        Quaternion qPitch = Quaternion.AngleAxis(pitch, Vector3.right);
+        Quaternion qHeading = Quaternion.AngleAxis(heading, Vector3.up);
+
+        Quaternion q = qHeading * qPitch * qRoll;
+        transform.rotation = q * transform.rotation; 
+
     }
 }
--- COMP2160 Demo Week 2/Assets/Scripts/InputAxes.cs
+++ COMP2160 Demo Week 2/Assets/Scripts/InputAxes.cs
@@ -1,6 +1,8 @@
 public class InputAxes
 {
     public const string Forward = "Forward";
-
+    public const string RotateRoll = "RotateRoll";
+    public const string RotatePitch = "RotatePitch";
+    public const string RotateHeading = "RotateHeading";
 
 }
